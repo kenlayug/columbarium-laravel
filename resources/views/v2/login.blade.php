af9ab13e2b51f28908cb248a454fa7c3f4b1bcd2
Ken Layug <ken_layug@yahoo.com>
2016-06-24 17:31:53 +0800

    added scripts and stylesheets in login blade.

--- resources/views/v2/login.blade.php
+++ resources/views/v2/login.blade.php
@@ -1,20 +1,27 @@
+<html>
+    <head>
+        @include('stylesheets')
+        @include('scripts')
 
+    </head>
 
-<body>
-    <div class = "aside aside col s6 z-depth-2">
+    <body>
+        <div class = "aside aside col s6 z-depth-2">
 
-        <div class = "row col s12" style = "margin-top: -20px; padding-left: 10px;">
-            <div class="input-field col s6">
-                <i class="material-icons prefix">email</i>
-                <input id="email" type="email" class="validate">
-                <label for="email" data-error="wrong" data-success="right">Email</label>
+            <div class = "row col s12" style = "margin-top: -20px; padding-left: 10px;">
+                <div class="input-field col s6">
+                    <i class="material-icons prefix">email</i>
+                    <input id="email" type="email" class="validate">
+                    <label for="email" data-error="wrong" data-success="right">Email</label>
+                </div>
+                <div class="input-field col s6">
+                    <i class="material-icons prefix">vpn_key</i>
+                    <input id="password" type="text" class="validate" required = "" aria-required="true" minlength = "1" maxlength="50" length = "50" pattern= "^[a-zA-Z'-\s]+|[0-9a-zA-Z'-\s]+|[a-zA-Z0-9'-]{1,20}">
+                    <label for="password" data-error = "Invalid format." data-success = "">Password<span style = "color: red;">*</span></label>
+                </div>
             </div>
-            <div class="input-field col s6">
-                <i class="material-icons prefix">vpn_key</i>
-                <input id="password" type="text" class="validate" required = "" aria-required="true" minlength = "1" maxlength="50" length = "50" pattern= "^[a-zA-Z'-\s]+|[0-9a-zA-Z'-\s]+|[a-zA-Z0-9'-]{1,20}">
-                <label for="password" data-error = "Invalid format." data-success = "">Password<span style = "color: red;">*</span></label>
-            </div>
+
         </div>
+    </body>
 
-    </div>
-</body>
+</html>
