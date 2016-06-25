020b2f0ae3a18e784216c0774fa775998c60991d
leoformaran <leoformaran012@gmail.com>
2016-06-24 20:19:41 -0800

    oks na ung login XD Hahaha

--- resources/views/v2/login.blade.php
+++ resources/views/v2/login.blade.php
@@ -1,25 +1,40 @@
 <html>
     <head>
+        <link rel = "stylesheet" href = "{!! asset('/css/loginUtilities.css') !!}"/>
         @include('stylesheets')
         @include('scripts')
 
     </head>
 
-    <body>
-        <div class = "aside aside col s6 z-depth-2">
+    <body style = "background-color: darkorange;">
+        <div class = "loginForm aside aside col s6 z-depth-2">
+            <img class = "responsive-img" id="image2" style="margin-top: -60px; margin-left: 35px; width: 290px; height: 290px;" src="{!! asset('/img/C&C-Logo-Final.png') !!}" alt="..." />
 
-            <div class = "row col s12" style = "margin-top: -20px; padding-left: 10px;">
-                <div class="input-field col s6">
+            <h4 style = "font-family: font; font-size: 1.5vw; margin-top: -40px; padding-left: 20px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Columbarium and Crematorium Management &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;System</h4>
+
+            <div class = "row col s12" style = "margin-right: 20px;margin-top: -20px; padding-left: 10px;">
+                <div class="input-field col s12">
                     <i class="material-icons prefix">email</i>
                     <input id="email" type="email" class="validate">
-                    <label for="email" data-error="wrong" data-success="right">Email</label>
+                    <label for="email" data-error="Invalid" data-success="">Email<span style = "color: red;">*</span></label>
                 </div>
-                <div class="input-field col s6">
+                <div class="input-field col s12">
                     <i class="material-icons prefix">vpn_key</i>
-                    <input id="password" type="text" class="validate" required = "" aria-required="true" minlength = "1" maxlength="50" length = "50" pattern= "^[a-zA-Z'-\s]+|[0-9a-zA-Z'-\s]+|[a-zA-Z0-9'-]{1,20}">
-                    <label for="password" data-error = "Invalid format." data-success = "">Password<span style = "color: red;">*</span></label>
+                    <input id="password" type="password" class="validate" required = "" aria-required="true" minlength = "1" maxlength="20" length = "20" pattern= "^[a-zA-Z'-\s]+|[0-9a-zA-Z'-\s]+|[a-zA-Z0-9'-]{1,20}">
+                    <label for="password" data-error = "Invalid Format." data-success = "">Password<span style = "color: red;">*</span></label>
                 </div>
             </div>
+
+            <div action = "#" style = "margin-top: -10px; margin-left: 30px;">
+                <p>
+                    <input type="checkbox" class="filled-in" id="filled-in-box"/>
+                    <label for="filled-in-box">Remember Password</label>
+                </p>
+            </div>
+
+            <button class="waves-effect waves-light teal btn" style = "width: 145px; color: white; margin-left: 20px;">Sign-up</button>
+            <button class="waves-effect waves-light teal btn" style = "width: 145px; color: white; margin-left: 10px; margin-right: 20px;">Login</button>
+
 
         </div>
     </body>
