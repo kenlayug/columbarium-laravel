ad5b41c7fa84393c12042701bc225920d4fbe037
kmyyyyyyyyy <kimberlybacarisas@gmail.com>
2016-07-11 00:07:10 +0800

    success in collection and downpayment added

--- resources/views/modals/collection-downpayment/downpaymentList.blade.php
+++ resources/views/modals/collection-downpayment/downpaymentList.blade.php
@@ -2,7 +2,7 @@
     <div id="admin" class="col s12">
         <div class="z-depth-2 card material-table" style="margin-left: 10px; margin-right: 10px;">
             <div class="table-header" style="background-color: #00897b;">
-                <h4 style = "font-size: 20px; color: white; padding-left: 0px; font-family: myFirstFont2">Transactions: Aaron Clyde Garil</h4>
+                <h4 style = "font-size: 20px; color: white; padding-left: 0px; font-family: myFirstFont">Transactions: Aaron Clyde Garil</h4>
                 <div class="actions">
                     <a href="#" class="search-toggle btn-flat nopadding"><i class="material-icons" style="color: #ffffff;">search</i></a>
                 </div>
---
+++ resources/views/modals/collection-downpayment/successDownpayment.blade.php
@@ -0,0 +1,92 @@
+<div id="generateReceiptDownpayment" class="modal modal-fixed-footer" style="width:75% !important; max-height: 100% !important; overflow-y: hidden;">
+
+    <div class="modal-header" style="padding: 0;">
+        <center><h4 style = "font-size: 20px;font-family: myFirstFont; color: white; padding: 20px;">Transaction Successfully Made!</h4></center>
+    </div>
+
+    <div class="modal-content" style="overflow-y: auto;">
+        <div class="row">
+
+            <div class="col s6">
+                <div class="row">
+                    <div class="col s4">
+                        <label style="color: #000000; font-size: 15px;">Customer Name:</label>
+                    </div>
+                    <div class="col s8">
+                        <label style="color: #000000; font-size: 15px;"><u>Aaron CLyde Garil</u></label>
+                    </div>
+                </div>
+                <div class="row" style="margin-top: -25px;">
+                    <div class="col s4">
+                        <label style="color: #000000; font-size: 15px;">Unit Code:</label>
+                    </div>
+                    <div class="col s8">
+                        <label style="color: #000000; font-size: 15px;"><u>A2</u></label>
+                    </div>
+                </div>
+                <div class="row" style="margin-top: -25px;">
+                    <div class="col s4">
+                        <label style="color: #000000; font-size: 15px;">Downpayment:</label>
+                    </div>
+                    <div class="col s8">
+                        <label style="color: #000000; font-size: 15px;"><u>P 9,000.00</u></label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s6">
+                <div class="row">
+                    <div class="col s4 offset-s6">
+                        <label style="color: #000000; font-size: 15px;">Transaction Code:</label>
+                    </div>
+                    <div class="col s2">
+                        <label style="color: #000000; font-size: 15px;"><u>T312</u></label>
+                    </div>
+                </div>
+                <div class="row" style="margin-top: -25px;">
+                    <div class="col s4 offset-s6">
+                        <label style="color: #000000; font-size: 15px;">Date:</label>
+                    </div>
+                    <div class="col s2">
+                        <label style="color: #000000; font-size: 15px;"><u>07/09/16</u></label>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col s8 offset-s2" style="border: 3px solid #7b7073;"><br>
+                <center><h6>Payment Details: </h6></center>
+                <div class="row">
+                    <div class="input-field col s7">
+                        <label>Downpayment:</label>
+                    </div>
+                    <div class="input-field col s5">
+                        <label><u>P 8,400.00</u></label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s7">
+                        <label>Amount Paid:</label>
+                    </div>
+                    <div class="input-field col s5">
+                        <label><u>P 4,400.00</u></label>
+                    </div>
+                </div>
+                <div class="row" style="border-top: 1px solid #7b7073; margin-top: 45px;">
+                    <div class="input-field col s7">
+                        <label>Balance:</label>
+                    </div>
+                    <div class="input-field col s5">
+                        <label><u style="color: red">P 4,000.00</u></label>
+                    </div><br><br><br>
+                </div>
+            </div>
+        </div>
+        <br><br>
+    </div>
+    <div class="modal-footer">
+        <button name = "action" class="waves-light btn light-green" style = "color: #000000;margin-left: 15px; margin-right: 15px">Generate Receipt</button>
+        <a name = "action" class="waves-light btn light-green modal-close" style="color: #000000;">Close</a>
+    </div>
+</div>
