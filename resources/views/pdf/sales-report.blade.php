366a197be30194e540116203f97ab33b326b226b
leoformaran <leoformaran012@gmail.com>
2016-08-25 09:49:37 -0800

    reports PDF XD

--- resources/views/pdf/sales-report.blade.php
+++ resources/views/pdf/sales-report.blade.php
@@ -1,139 +1,142 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
+<style>
+    #logo{
+        margin-top: -20px;
+        margin-left: -25px;
+        width: 150px;
+        height: 150px;
+        position: absolute;
+        z-index: 99;
+    }
+    span {
+        font-weight: normal;
+    }
+    th {
+        font-size: 13px;
+    }
+
+    table {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+        margin-top: 10px;
+    }
+    td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+    }
+    tr:nth-child(even) {
+        background-color: #dddddd;
+    }
+    td {
+        font-weight: bold;
+    }
+    span {
+        font-weight: normal;
+    }
+    h3 {
+        margin-top: 30px;
+    }
+    h4 {
+        padding-top: -20px;
+    }
+    body {
+        font-family: "Arial Narrow";
+    }
+    .margin {
+        padding-top: -20px;
+    }
+    .date {
+        padding-top: -20px;
+    }
+</style>
 
 
-    <style>
-        * {
-            font-family: roboto3;
-        }
-
-        #logo{
-            margin-top: -20px;
-            margin-left: -15px;
-            width: 150px;
-            height: 150px;
-        }
-
-        span {
-            font-weight: normal;
-        }
-
-        .main td {
-            border-right: solid 3px dimgrey;
-            border-left: solid 3px dimgrey;
-        }
-
-        .main th {
-            border-right: solid 3px dimgrey;
-            border-left: solid 3px dimgrey;
-        }
-
-        .unitTable td {
-            border-right: solid 3px dimgrey;
-            border-left: solid 3px dimgrey;
-        }
-
-        .unitTable th {
-            border-right: solid 3px dimgrey;
-            border-left: solid 3px dimgrey;
-        }
-
-
-        table {
-            border-collapse: collapse;
-            border: 3px solid dimgrey;
-            width: 90%;
-        }
-        th {
-            height: 40px;
-            font-size: 18px;
-        }
-
-        hr {
-            width: 95%;
-        }
-
-        .unitTable {
-            margin-top: -30px;
-        }
-
-        .label {
-            border: none;
-            width: 140%;
-            margin-left: 67px;
-        }
-
-        td {
-            font-weight: bold;
-        }
-
-        span {
-            font-weight: normal;
-        }
-
-        h3 {
-            margin-top: -80px;
-        }
-
-        h4 {
-            padding-top: -20px;
-        }
-    </style>
-
-
-
-</head>
 <body>
 <img id="logo" src="{!! public_path('img/C&C-Logo-Final2.png') !!}">
-<h3 style="text-align: center">Columbarium and Crematorium Management System</h3>
-<h4 align = "center" style = "margin-top: -30px; padding-top: -20px;">Sta. Mesa, Manila</h4>
+<h3 align = "center" style = "font-family: roboto3">Columbarium and Crematorium Management System</h3>
+<h4 align = "center">Sta. Mesa, Manila</h4>
 <h4 align = "center">Tel No: 02-364 0158</h4>
 
-<h2 align = "center">Add Deceased Receipt</h2>
-
-<table class = "label">
-    <tr align = "left">
-        <td>Customer Name:&nbsp;<span>Leyooo</span></td>
-        <td>Transaction Code:&nbsp;<span>T001</span></td>
+<h2 align = "center">Sales Report</h2>
+<h5 class = "date" align = "center">August 26, 2016</h5>
+<h5 class = "margin2" align = "left">To:&nbsp;<span>January 1, 2016</span></h5>
+<h5 class = "margin" align = "left">From:&nbsp;<span>December 31, 2016</span></h5>
+<table>
+    <tr>
+        <th>Date</th>
+        <th>Customer Name</th>
+        <th>Transaction ID</th>
+        <th>Category</th>
+        <th>Name</th>
+        <th>Price</th>
+        <th>Quantity</th>
+        <th>Total Price</th>
     </tr>
-    <tr align = "left">
+    <tr>
         <td></td>
-        <td>Date:&nbsp;<span>July 31, 2016</span></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
     </tr>
 </table>
 
 <br>
-
-<table class = "main" align = "center">
-      <tr>
-            <th>Added Deceased Details:</th>
-            <th>Payment Details:</th>
-          </tr>
-    <tr align = "left">
-        <td>Deceased Name:&nbsp; <span>Juan Dela Cruz</span></td>
-        <td>Service Fee:&nbsp; <span>P 1,500.00</span></td>
-    </tr>
-    <tr align = "left">
-        <td>Date of Death:&nbsp; <span>Sunday, July 3, 2016</span></td>
-        <td>Amount Paid:&nbsp; <span>P 2,000.00</span><hr></td>
-    </tr>
-    <tr>
-        <td align = "left">Storage Type:&nbsp; <span>Urn</span></td>
-        <td align = "center">Change:&nbsp; <span>P 500.00</span></td>
-    </tr>
-    <tr align = "left">
-        <td>Service:&nbsp; <span>Internment</span></td>
-        <td></td>
-    </tr>
-    <tr align = "left">
-        <td>Service Fee:&nbsp; <span>P 1,500.00</span></td>
-        <td></td>
-    </tr>
-</table>
-
+<h5 align = "right">Total Number of Transactions:&nbsp;<span>5</span></h5>
+<h5 class = "margin" align = "right">Total Sales:&nbsp;<span>P 50,000.00</span></h5>
 
 </body>
-</html>
--- resources/views/pdf/schedule.blade.php
+++ resources/views/pdf/schedule.blade.php
@@ -1,1 +1,113 @@
-List of Schedule
+<style>
+    #logo{
+        margin-top: -20px;
+        margin-left: -25px;
+        width: 150px;
+        height: 150px;
+        position: absolute;
+        z-index: 99;
+    }
+    span {
+        font-weight: normal;
+    }
+    th {
+        font-size: 13px;
+    }
+
+    table {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+        margin-top: 10px;
+    }
+    td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+    }
+    tr:nth-child(even) {
+        background-color: #dddddd;
+    }
+    td {
+        font-weight: bold;
+    }
+    span {
+        font-weight: normal;
+    }
+    h3 {
+        margin-top: 30px;
+    }
+    h4 {
+        padding-top: -20px;
+    }
+    body {
+        font-family: "Arial Narrow";
+    }
+    .margin {
+        padding-top: -20px;
+    }
+    .date {
+        padding-top: -20px;
+    }
+</style>
+
+
+<body>
+<img id="logo" src="{!! public_path('img/C&C-Logo-Final2.png') !!}">
+<h3 align = "center" style = "font-family: roboto3">Columbarium and Crematorium Management System</h3>
+<h4 align = "center">Sta. Mesa, Manila</h4>
+<h4 align = "center">Tel No: 02-364 0158</h4>
+
+<h2 align = "center">List of Schedule Report</h2>
+<h5 class = "date" align = "center">August 26, 2016</h5>
+<h5 class = "margin2" align = "left">To:&nbsp;<span>January 1, 2016</span></h5>
+<h5 class = "margin" align = "left">From:&nbsp;<span>December 31, 2016</span></h5>
+<table>
+    <tr>
+        <th>Date</th>
+        <th>Customer Name</th>
+        <th>Service/Additional</th>
+        <th>Time</th>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
+
+<br>
+<h5 align = "right">Total Number of Schedule/s:&nbsp;<span>5</span></h5>
+
+</body>
